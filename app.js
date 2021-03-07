49b2673fd5b70762643326441862b9bc5e8a8ca4
Ghaith Obeidat <ghaith.ob85@gmail.com>
2021-03-07 14:01:42 +0200

    Gaming

---
+++ app.js
@@ -0,0 +1,21 @@
+var Game=prompt("What game would you like to purchase : GOW, RDR2?");
+
+while (Game !='GOW' && Game !='RDR2')
+{
+    Game=prompt('Out of stock');
+}
+var copies=prompt('How many copies do you want?');
+var order ='' ;
+if (Game=="GOW") 
+{
+    order= '<img src="GOW.jpg"/>';
+}
+else {
+    order= '<img src="RDR2.jpg"/>';
+}
+
+for (var i=0 ; i < copies ; i++)
+{
+document.write(order);
+} 
+console.log(order);
